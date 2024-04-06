b41b9cac4ee028ed46dc7e9d032832cce5d24c33
Axel Brisse <axel.brisse@gmail.com>
2024-04-06 05:31:48 +0200

    remove pretty much all rust code

--- v1/src/lib.rs
+++ v1/src/lib.rs
@@ -4,23 +4,33 @@
 
 use core::{arch::asm, panic::PanicInfo};
 
-mod entry;
-mod idt;
-mod interrupts;
-mod pic;
-mod port;
-mod vga_buffer;
-
 #[no_mangle]
 pub extern "C" fn kernel_main() {
-    println!("KERNEL");
+    let hello = b"Hello World!";
+    let color_byte = 0x1f;
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i * 2] = *char_byte;
+    }
+    let buffer_ptr = (0xb8000 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
+    panic!("Forced panic for testing");
+
     hlt_loop()
 }
 
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    // TODO: Yellow on Black
-    println!("{}", info);
+fn panic(_info: &PanicInfo) -> ! {
+    let hello = b"Panic Panic!";
+    let color_byte = 0xe0;
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i * 2] = *char_byte;
+    }
+    let buffer_ptr = (0xb8000 + 2308) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+
     hlt_loop()
 }
 
