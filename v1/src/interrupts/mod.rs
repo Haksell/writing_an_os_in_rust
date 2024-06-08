17f55c8de6cca67f355ce0d88c46fc6840b4f13d
Axel Brisse <axel.brisse@gmail.com>
2024-06-08 21:14:26 +0200

    ### The final Steps. nearly done but my eyes hurt and I cant find set_cs

--- v1/src/interrupts/mod.rs
+++ v1/src/interrupts/mod.rs
@@ -1,9 +1,13 @@
 mod gdt;
 
 use crate::memory::MemoryController;
+use gdt::Gdt;
 use lazy_static::lazy_static;
+use spin::Once;
 use x86_64::{
+    instructions::tables::load_tss,
     structures::{
+        gdt::SegmentSelector,
         idt::{InterruptDescriptorTable, InterruptStackFrame},
         tss::TaskStateSegment,
     },
@@ -21,13 +25,37 @@ lazy_static! {
     };
 }
 
+static TSS: Once<TaskStateSegment> = Once::new();
+static GDT: Once<(Gdt, SegmentSelector, SegmentSelector)> = Once::new();
+
 pub fn init(memory_controller: &mut MemoryController) {
     let double_fault_stack = memory_controller
         .alloc_stack(1)
         .expect("could not allocate double fault stack");
-    let mut tss = TaskStateSegment::new();
-    tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX] =
-        VirtAddr::new(double_fault_stack.top() as u64);
+    let tss = TSS.call_once(|| {
+        let mut tss = TaskStateSegment::new();
+        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX] =
+            VirtAddr::new(double_fault_stack.top() as u64);
+        tss
+    });
+
+    // https://os.phil-opp.com/double-faults/#the-final-steps
+
+    let (gdt, code_selector, tss_selector) = GDT.call_once(|| {
+        let mut gdt = Gdt::new();
+        (
+            gdt,
+            gdt.add_entry(gdt::Descriptor::kernel_code_segment()),
+            gdt.add_entry(gdt::Descriptor::tss_segment(&tss)),
+        )
+    });
+    gdt.load();
+    unsafe {
+        set_cs(code_selector); // reload code segment register
+        load_tss(*tss_selector);
+    }
+    println!("GDT loaded.");
+
     IDT.load();
     println!("IDT loaded.");
 }
