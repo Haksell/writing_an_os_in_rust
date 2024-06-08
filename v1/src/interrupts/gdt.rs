98c7bbcdb86730a000ff864d00f371e38e5bbb40
Axel Brisse <axel.brisse@gmail.com>
2024-06-08 20:47:34 +0200

    https://os.phil-opp.com/double-faults/\#adding-descriptors-to-the-gdt

--- v1/src/interrupts/gdt.rs
+++ v1/src/interrupts/gdt.rs
@@ -1,4 +1,8 @@
+use core::mem::size_of;
+
+use bit_field::BitField as _;
 use bitflags::bitflags;
+use x86_64::structures::tss::TaskStateSegment;
 
 pub enum Descriptor {
     UserSegment(u64),
@@ -21,7 +25,20 @@ impl Descriptor {
             | DescriptorFlags::PRESENT
             | DescriptorFlags::EXECUTABLE
             | DescriptorFlags::LONG_MODE;
-        Descriptor::UserSegment(flags.bits())
+        Self::UserSegment(flags.bits())
+    }
+
+    /// segment segment?
+    pub fn tss_segment(tss: &'static TaskStateSegment) -> Self {
+        let ptr = tss as *const _ as u64;
+        let mut low = DescriptorFlags::PRESENT.bits();
+        low.set_bits(16..40, ptr.get_bits(0..24));
+        low.set_bits(56..64, ptr.get_bits(24..32));
+        low.set_bits(0..16, (size_of::<TaskStateSegment>() - 1) as u64);
+        low.set_bits(40..44, 0b1001); // type (0b1001 = available 64-bit tss)
+        let mut high = 0;
+        high.set_bits(0..32, ptr.get_bits(32..64));
+        Self::SystemSegment(low, high)
     }
 }
 
