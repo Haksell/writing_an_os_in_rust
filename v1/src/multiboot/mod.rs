7ff8e2f5024fe21e3b86338162a82d8ee3057379
Axel Brisse <axel.brisse@gmail.com>
2024-06-15 06:29:33 +0200

    .

--- v1/src/multiboot/mod.rs
+++ v1/src/multiboot/mod.rs
@@ -10,7 +10,6 @@ use self::elf_sections::{ElfSectionIter, ElfSectionsTag};
 use self::memory_map::MemoryMapTag;
 use self::tag::{Tag, TagIter, TagTrait};
 use self::tag_type::TagType;
-use core::mem::size_of;
 
 pub struct BootInformation {
     pub start_address: usize,
@@ -20,13 +19,11 @@ pub struct BootInformation {
 
 impl BootInformation {
     pub unsafe fn load(multiboot_address: usize) -> Self {
-        let total_size = *(multiboot_address as *const u32);
-        let tags_ptr = (multiboot_address + size_of::<u32>() * 2) as *const u8;
-        let slice_size = total_size as usize - size_of::<u32>() * 2;
+        let total_size = *(multiboot_address as *const u32) as usize;
         Self {
             start_address: multiboot_address,
-            end_address: multiboot_address + total_size as usize,
-            tags: core::slice::from_raw_parts(tags_ptr, slice_size),
+            end_address: multiboot_address + total_size,
+            tags: core::slice::from_raw_parts((multiboot_address + 8) as *const u8, total_size - 8),
         }
     }
 
