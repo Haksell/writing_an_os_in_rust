98c1fe6bec7061236146dbcdc5d320f38cb95282
Axel Brisse <axel.brisse@gmail.com>
2024-06-29 08:15:21 +0200

    .

--- v1/src/xxx/structures/paging/frame.rs
+++ v1/src/xxx/structures/paging/frame.rs
@@ -1,6 +1,5 @@
 use crate::xxx::structures::paging::page::{PageSize, Size4KiB};
 use crate::xxx::PhysAddr;
-use core::fmt;
 use core::marker::PhantomData;
 use core::ops::{Add, AddAssign, Sub, SubAssign};
 
@@ -23,16 +22,6 @@ impl<S: PageSize> PhysFrame<S> {
     #[inline]
     pub fn start_address(self) -> PhysAddr {
         self.start_address
-    }
-}
-
-impl<S: PageSize> fmt::Debug for PhysFrame<S> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_fmt(format_args!(
-            "PhysFrame[{}]({:#x})",
-            S::DEBUG_STR,
-            self.start_address().as_u64()
-        ))
     }
 }
 
@@ -96,15 +85,6 @@ impl<S: PageSize> Iterator for PhysFrameRange<S> {
     }
 }
 
-impl<S: PageSize> fmt::Debug for PhysFrameRange<S> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("PhysFrameRange")
-            .field("start", &self.start)
-            .field("end", &self.end)
-            .finish()
-    }
-}
-
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(C)]
 pub struct PhysFrameRangeInclusive<S: PageSize = Size4KiB> {
@@ -124,14 +104,5 @@ impl<S: PageSize> Iterator for PhysFrameRangeInclusive<S> {
         } else {
             None
         }
-    }
-}
-
-impl<S: PageSize> fmt::Debug for PhysFrameRangeInclusive<S> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("PhysFrameRangeInclusive")
-            .field("start", &self.start)
-            .field("end", &self.end)
-            .finish()
     }
 }
