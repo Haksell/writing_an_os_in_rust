0f28411c601c9a6b1e78a9d72677d0759664c8c9
Axel Brisse <axel.brisse@gmail.com>
2024-06-29 09:57:33 +0200

    .

--- v1/src/xxx/instructions/segmentation.rs
+++ v1/src/xxx/instructions/segmentation.rs
@@ -1,19 +1,13 @@
 use crate::xxx::registers::segmentation::{Segment, SegmentSelector, CS};
 use core::arch::asm;
 
-macro_rules! segment_impl {
-    ($type:ty, $name:literal) => {
-        impl Segment for $type {
-            #[inline]
-            fn get_reg() -> SegmentSelector {
-                let segment: u16;
-                unsafe {
-                    asm!(concat!("mov {0:x}, ", $name), out(reg) segment, options(nomem, nostack, preserves_flags));
-                }
-                SegmentSelector(segment)
-            }
+impl Segment for CS {
+    #[inline]
+    fn get_reg() -> SegmentSelector {
+        let segment: u16;
+        unsafe {
+            asm!(concat!("mov {0:x}, cs"), out(reg) segment, options(nomem, nostack, preserves_flags));
         }
-    };
+        SegmentSelector(segment)
+    }
 }
-
-segment_impl!(CS, "cs");
