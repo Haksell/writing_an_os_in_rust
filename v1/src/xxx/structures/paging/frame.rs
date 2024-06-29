306805682e543be390a37ebc0132ed09c493a2b8
Axel Brisse <axel.brisse@gmail.com>
2024-06-29 08:21:42 +0200

    .

--- v1/src/xxx/structures/paging/frame.rs
+++ v1/src/xxx/structures/paging/frame.rs
@@ -1,7 +1,6 @@
 use crate::xxx::structures::paging::page::{PageSize, Size4KiB};
 use crate::xxx::PhysAddr;
 use core::marker::PhantomData;
-use core::ops::{Add, AddAssign, Sub, SubAssign};
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[repr(C)]
@@ -22,87 +21,5 @@ impl<S: PageSize> PhysFrame<S> {
     #[inline]
     pub fn start_address(self) -> PhysAddr {
         self.start_address
-    }
-}
-
-impl<S: PageSize> Add<u64> for PhysFrame<S> {
-    type Output = Self;
-    #[inline]
-    fn add(self, rhs: u64) -> Self::Output {
-        PhysFrame::containing_address(self.start_address() + rhs * S::SIZE)
-    }
-}
-
-impl<S: PageSize> AddAssign<u64> for PhysFrame<S> {
-    #[inline]
-    fn add_assign(&mut self, rhs: u64) {
-        *self = *self + rhs;
-    }
-}
-
-impl<S: PageSize> Sub<u64> for PhysFrame<S> {
-    type Output = Self;
-    #[inline]
-    fn sub(self, rhs: u64) -> Self::Output {
-        PhysFrame::containing_address(self.start_address() - rhs * S::SIZE)
-    }
-}
-
-impl<S: PageSize> SubAssign<u64> for PhysFrame<S> {
-    #[inline]
-    fn sub_assign(&mut self, rhs: u64) {
-        *self = *self - rhs;
-    }
-}
-
-impl<S: PageSize> Sub<PhysFrame<S>> for PhysFrame<S> {
-    type Output = u64;
-    #[inline]
-    fn sub(self, rhs: PhysFrame<S>) -> Self::Output {
-        (self.start_address - rhs.start_address) / S::SIZE
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-#[repr(C)]
-pub struct PhysFrameRange<S: PageSize = Size4KiB> {
-    pub start: PhysFrame<S>,
-    pub end: PhysFrame<S>,
-}
-
-impl<S: PageSize> Iterator for PhysFrameRange<S> {
-    type Item = PhysFrame<S>;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.start < self.end {
-            let frame = self.start;
-            self.start += 1;
-            Some(frame)
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
-#[repr(C)]
-pub struct PhysFrameRangeInclusive<S: PageSize = Size4KiB> {
-    pub start: PhysFrame<S>,
-    pub end: PhysFrame<S>,
-}
-
-impl<S: PageSize> Iterator for PhysFrameRangeInclusive<S> {
-    type Item = PhysFrame<S>;
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.start <= self.end {
-            let frame = self.start;
-            self.start += 1;
-            Some(frame)
-        } else {
-            None
-        }
     }
 }
