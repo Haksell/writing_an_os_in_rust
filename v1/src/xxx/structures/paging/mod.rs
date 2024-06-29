ee22bb11f71eb662af1c4042a59bb79822433403
Axel Brisse <axel.brisse@gmail.com>
2024-06-29 07:55:26 +0200

    .

--- v1/src/xxx/structures/paging/mod.rs
+++ v1/src/xxx/structures/paging/mod.rs
@@ -1,9 +1,8 @@
 pub use self::frame::PhysFrame;
-pub use self::page::{Page, PageSize, Size1GiB, Size2MiB, Size4KiB};
+pub use self::page::{PageSize, Size1GiB, Size2MiB, Size4KiB};
 pub use self::page_table::{PageOffset, PageTableIndex};
 
 pub mod frame;
 mod frame_alloc;
-pub mod mapper;
 pub mod page;
 pub mod page_table;
