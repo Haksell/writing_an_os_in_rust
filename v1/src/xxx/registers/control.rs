f7c7b764e07ac224c1b8621717af916ab5306330
Axel Brisse <axel.brisse@gmail.com>
2024-06-29 08:52:30 +0200

    .

--- v1/src/xxx/registers/control.rs
+++ v1/src/xxx/registers/control.rs
@@ -1,4 +1,6 @@
+use crate::xxx::{structures::paging::PhysFrame, PhysAddr};
 use bitflags::bitflags;
+use core::arch::asm;
 
 #[derive(Debug)]
 pub struct Cr0;
@@ -21,6 +23,42 @@ bitflags! {
     }
 }
 
+impl Cr0 {
+    #[inline]
+    pub fn read() -> Cr0Flags {
+        Cr0Flags::from_bits_truncate(Self::read_raw())
+    }
+
+    #[inline]
+    pub fn read_raw() -> u64 {
+        let value: u64;
+
+        unsafe {
+            asm!("mov {}, cr0", out(reg) value, options(nomem, nostack, preserves_flags));
+        }
+
+        value
+    }
+
+    #[inline]
+    pub unsafe fn write(flags: Cr0Flags) {
+        let old_value = Self::read_raw();
+        let reserved = old_value & !(Cr0Flags::all().bits());
+        let new_value = reserved | flags.bits();
+
+        unsafe {
+            Self::write_raw(new_value);
+        }
+    }
+
+    #[inline]
+    pub unsafe fn write_raw(value: u64) {
+        unsafe {
+            asm!("mov cr0, {}", in(reg) value, options(nostack, preserves_flags));
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Cr3;
 
@@ -32,83 +70,41 @@ bitflags! {
     }
 }
 
-mod x86_64 {
-    use super::*;
-    use crate::xxx::{structures::paging::PhysFrame, PhysAddr};
-    use core::arch::asm;
+impl Cr3 {
+    #[inline]
+    pub fn read() -> (PhysFrame, Cr3Flags) {
+        let (frame, value) = Cr3::read_raw();
+        let flags = Cr3Flags::from_bits_truncate(value.into());
+        (frame, flags)
+    }
 
-    impl Cr0 {
-        #[inline]
-        pub fn read() -> Cr0Flags {
-            Cr0Flags::from_bits_truncate(Self::read_raw())
+    #[inline]
+    pub fn read_raw() -> (PhysFrame, u16) {
+        let value: u64;
+
+        unsafe {
+            asm!("mov {}, cr3", out(reg) value, options(nomem, nostack, preserves_flags));
         }
 
-        #[inline]
-        pub fn read_raw() -> u64 {
-            let value: u64;
+        let addr = PhysAddr::new(value & 0x_000f_ffff_ffff_f000);
+        let frame = PhysFrame::containing_address(addr);
+        (frame, (value & 0xFFF) as u16)
+    }
 
-            unsafe {
-                asm!("mov {}, cr0", out(reg) value, options(nomem, nostack, preserves_flags));
-            }
-
-            value
-        }
-
-        #[inline]
-        pub unsafe fn write(flags: Cr0Flags) {
-            let old_value = Self::read_raw();
-            let reserved = old_value & !(Cr0Flags::all().bits());
-            let new_value = reserved | flags.bits();
-
-            unsafe {
-                Self::write_raw(new_value);
-            }
-        }
-
-        #[inline]
-        pub unsafe fn write_raw(value: u64) {
-            unsafe {
-                asm!("mov cr0, {}", in(reg) value, options(nostack, preserves_flags));
-            }
+    #[inline]
+    pub unsafe fn write(frame: PhysFrame, flags: Cr3Flags) {
+        unsafe {
+            Cr3::write_raw_impl(false, frame, flags.bits() as u16);
         }
     }
 
-    impl Cr3 {
-        #[inline]
-        pub fn read() -> (PhysFrame, Cr3Flags) {
-            let (frame, value) = Cr3::read_raw();
-            let flags = Cr3Flags::from_bits_truncate(value.into());
-            (frame, flags)
-        }
+    #[inline]
+    unsafe fn write_raw_impl(top_bit: bool, frame: PhysFrame, val: u16) {
+        let addr = frame.start_address();
+        let value = ((top_bit as u64) << 63) | addr.as_u64() | val as u64;
 
-        #[inline]
-        pub fn read_raw() -> (PhysFrame, u16) {
-            let value: u64;
-
-            unsafe {
-                asm!("mov {}, cr3", out(reg) value, options(nomem, nostack, preserves_flags));
-            }
-
-            let addr = PhysAddr::new(value & 0x_000f_ffff_ffff_f000);
-            let frame = PhysFrame::containing_address(addr);
-            (frame, (value & 0xFFF) as u16)
-        }
-
-        #[inline]
-        pub unsafe fn write(frame: PhysFrame, flags: Cr3Flags) {
-            unsafe {
-                Cr3::write_raw_impl(false, frame, flags.bits() as u16);
-            }
-        }
-
-        #[inline]
-        unsafe fn write_raw_impl(top_bit: bool, frame: PhysFrame, val: u16) {
-            let addr = frame.start_address();
-            let value = ((top_bit as u64) << 63) | addr.as_u64() | val as u64;
-
-            unsafe {
-                asm!("mov cr3, {}", in(reg) value, options(nostack, preserves_flags));
-            }
+        unsafe {
+            asm!("mov cr3, {}", in(reg) value, options(nostack, preserves_flags));
         }
     }
 }
--- v1/src/xxx/registers/model_specific.rs
+++ v1/src/xxx/registers/model_specific.rs
@@ -1,4 +1,4 @@
-use bitflags::bitflags;
+use core::arch::asm;
 
 #[derive(Debug)]
 pub struct Msr(u32);
@@ -10,68 +10,33 @@ impl Msr {
     }
 }
 
-bitflags! {
-    #[repr(transparent)]
-    #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone, Copy)]
-    pub struct EferFlags: u64 {
-        const SYSTEM_CALL_EXTENSIONS = 1;
-        const LONG_MODE_ENABLE = 1 << 8;
-        const LONG_MODE_ACTIVE = 1 << 10;
-        const NO_EXECUTE_ENABLE = 1 << 11;
-        const SECURE_VIRTUAL_MACHINE_ENABLE = 1 << 12;
-        const LONG_MODE_SEGMENT_LIMIT_ENABLE = 1 << 13;
-        const FAST_FXSAVE_FXRSTOR = 1 << 14;
-        const TRANSLATION_CACHE_EXTENSION = 1 << 15;
-    }
-}
-
-bitflags! {
-    #[repr(transparent)]
-    #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone, Copy)]
-    pub struct CetFlags: u64 {
-        const SS_ENABLE = 1 << 0;
-        const SS_WRITE_ENABLE = 1 << 1;
-        const IBT_ENABLE = 1 << 2;
-        const IBT_LEGACY_ENABLE = 1 << 3;
-        const IBT_NO_TRACK_ENABLE = 1 << 4;
-        const IBT_LEGACY_SUPPRESS_ENABLE = 1 << 5;
-        const IBT_SUPPRESS_ENABLE = 1 << 10;
-        const IBT_TRACKED = 1 << 11;
-    }
-}
-
-mod x86_64 {
-    use super::*;
-    use core::arch::asm;
-
-    impl Msr {
-        #[inline]
-        pub unsafe fn read(&self) -> u64 {
-            let (high, low): (u32, u32);
-            unsafe {
-                asm!(
-                    "rdmsr",
-                    in("ecx") self.0,
-                    out("eax") low, out("edx") high,
-                    options(nomem, nostack, preserves_flags),
-                );
-            }
-            ((high as u64) << 32) | (low as u64)
+impl Msr {
+    #[inline]
+    pub unsafe fn read(&self) -> u64 {
+        let (high, low): (u32, u32);
+        unsafe {
+            asm!(
+                "rdmsr",
+                in("ecx") self.0,
+                out("eax") low, out("edx") high,
+                options(nomem, nostack, preserves_flags),
+            );
         }
+        ((high as u64) << 32) | (low as u64)
+    }
 
-        #[inline]
-        pub unsafe fn write(&mut self, value: u64) {
-            let low = value as u32;
-            let high = (value >> 32) as u32;
+    #[inline]
+    pub unsafe fn write(&mut self, value: u64) {
+        let low = value as u32;
+        let high = (value >> 32) as u32;
 
-            unsafe {
-                asm!(
-                    "wrmsr",
-                    in("ecx") self.0,
-                    in("eax") low, in("edx") high,
-                    options(nostack, preserves_flags),
-                );
-            }
+        unsafe {
+            asm!(
+                "wrmsr",
+                in("ecx") self.0,
+                in("eax") low, in("edx") high,
+                options(nostack, preserves_flags),
+            );
         }
     }
 }
