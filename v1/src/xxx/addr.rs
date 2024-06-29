d2909a56325e3fb8b35a54a63bd246a46404b271
Axel Brisse <axel.brisse@gmail.com>
2024-06-29 09:51:03 +0200

    .

--- v1/src/xxx/addr.rs
+++ v1/src/xxx/addr.rs
@@ -5,26 +5,13 @@ pub struct VirtAddr(u64);
 impl VirtAddr {
     #[inline]
     pub const fn new(addr: u64) -> VirtAddr {
-        // TODO: fix addresses that should be sign-extended with 1
-        match Self::try_new(addr) {
-            Some(v) => v,
-            None => panic!("virtual address must be sign extended in bits 48 to 64"),
-        }
-    }
-
-    #[inline]
-    pub const fn try_new(addr: u64) -> Option<VirtAddr> {
-        let v = Self::new_truncate(addr);
-        if v.0 == addr {
-            Some(v)
+        // TODO: fix for addresses that should be sign-extended with 1
+        let truncated = ((addr << 16) as i64 >> 16) as u64;
+        if truncated == addr {
+            VirtAddr(truncated)
         } else {
-            None
+            panic!("virtual address must be sign extended in bits 48 to 64");
         }
-    }
-
-    #[inline]
-    pub const fn new_truncate(addr: u64) -> VirtAddr {
-        VirtAddr(((addr << 16) as i64 >> 16) as u64)
     }
 
     #[inline]
