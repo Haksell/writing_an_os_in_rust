c7ff3c24fd06e6bceaca0837011408b9be73c798
Axel Brisse <axel.brisse@gmail.com>
2024-06-29 09:20:27 +0200

    .

--- v1/src/xxx/structures/paging/page.rs
+++ v1/src/xxx/structures/paging/page.rs
@@ -1,6 +1,3 @@
-use crate::xxx::addr::VirtAddr;
-use core::marker::PhantomData;
-
 pub trait PageSize: Copy + Eq + PartialOrd + Ord {
     const SIZE: u64;
 }
@@ -10,11 +7,4 @@ pub enum Size4KiB {}
 
 impl PageSize for Size4KiB {
     const SIZE: u64 = 4096;
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[repr(C)]
-pub struct Page<S: PageSize = Size4KiB> {
-    start_address: VirtAddr,
-    size: PhantomData<S>,
 }
