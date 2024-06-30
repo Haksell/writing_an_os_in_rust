c0fa08645789a1daae7721c543ab4e02ec95c5c2
Axel Brisse <axel.brisse@gmail.com>
2024-06-30 10:52:19 +0200

    no stack overflow

--- v1/src/lib.rs
+++ v1/src/lib.rs
@@ -49,11 +49,6 @@ pub extern "C" fn kernel_main(multiboot_start: usize) {
 
     interrupts::init(&mut memory_controller);
 
-    // fn stack_overflow() {
-    //     stack_overflow();
-    // }
-    // stack_overflow();
-
     println!("No crash! \x02");
     hlt_loop()
 }
